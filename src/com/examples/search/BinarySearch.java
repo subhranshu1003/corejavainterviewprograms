ce310dcf65ed583d6b93e4331fc1db6ca1a8ba3c
Subhranshu Pujari <subh.pujari@gmail.com>
2020-06-16 12:19:29 +0530

    adding proper comments

--- src/com/examples/search/BinarySearch.java
+++ src/com/examples/search/BinarySearch.java
@@ -1,4 +1,12 @@
-//Search using Binary search algorithm
+/*Search using Binary search algorithm
+ * We basically ignore half of the elements just after one comparison.
+ *
+ *  Compare x with the middle element.
+ *	If x matches with middle element, we return the mid index.
+ *	Else If x is greater than the mid element, then x can only lie in right half subarray after the mid element. So we recur for right half.
+ *	Else (x is smaller) recur for the left half.
+ */
+
 package com.examples.search;
 
 import java.util.Scanner;
--- src/com/examples/search/LinearSearch.java
+++ src/com/examples/search/LinearSearch.java
@@ -1,4 +1,9 @@
-//Linear search 
+/*Linear search 
+ * A simple approach is to do linear search, i.e
+ * Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
+ *	If x matches with an element, return the index.
+ *  If x doesn’t match with any of elements, return -1.
+ */
 package com.examples.search;
 
 import java.util.Scanner;
