79ecaef9223f4cf30a86cd66489cbc900354cae2
Subhranshu Pujari <subh.pujari@gmail.com>
2020-06-16 12:22:27 +0530

    adding proper comments

--- src/com/examples/search/BinarySearch.java
+++ src/com/examples/search/BinarySearch.java
@@ -1,4 +1,11 @@
 /*Search using Binary search algorithm
+ * 
+ *  Search a sorted array by repeatedly dividing the search interval in half. 
+ * Begin with an interval covering the whole array. 
+ * If the value of the search key is less than the item in the middle of the interval, 
+ * narrow the interval to the lower half. Otherwise narrow it to the upper half.
+ *  Repeatedly check until the value is found or the interval is empty
+ *  
  * We basically ignore half of the elements just after one comparison.
  *
  *  Compare x with the middle element.
