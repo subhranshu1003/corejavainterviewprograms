7d3f387a9f311751243eb8bd0405d38236a723b2
Subhranshu Pujari <subh.pujari@gmail.com>
2020-06-16 10:09:44 +0530

    linear search example

---
+++ src/com/examples/search/LinearSearch.java
@@ -0,0 +1,36 @@
+//Linear search 
+package com.examples.search;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+	public int search(int[] arr, int x) {
+		// edge case
+		
+		if(arr == null || arr.length == 0) {
+			throw new IllegalArgumentException("Invalid input");
+		}
+		int n=arr.length;
+		for(int i = 0; i < n; i++) {
+			if(arr[i] == x) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int[] arr = {5, 1, 9, 2, 10, 15, 20};
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number to search:");
+		int n = scanner.nextInt();
+		scanner.close();// it is the number to be checked
+		LinearSearch ls = new LinearSearch();
+		int result=ls.search(arr, n);
+		if(result==-1) {
+			System.out.println("The input number is not present in array");
+		}
+		else {
+			System.out.println("The input number is  present in array");
+		}
+	}
+}
